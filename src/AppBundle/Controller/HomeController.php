00f49595089dba241c1dc4d784ac2f9a1b50db05
Valeria Antonova <valeria.antonia001@gmail.com>
2018-04-06 20:25:41 +0600

    1. Добавлен контроллер главной страницы с методом show 2. Добавлен шаблон пока что общего вида главной страницы 3. Удален git.ignore

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: valeria
+ * Date: 4/6/18
+ * Time: 8:04 PM
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/home")
+     */
+    public function showAction()
+    {
+        $twig = $this->container->get('twig');
+        $html = $twig->render('home/show.html.twig');
+        return new Response($html);
+    }
+}
---
+++ var/cache/dev/twig/e0/e0f747fcc1c2abd45de4c21a6cb363db023c9a284749afe2df4d0ea13ba576db.php
@@ -0,0 +1,408 @@
+<?php
+
+/* home/show.html.twig */
+class __TwigTemplate_006c81674b25aef45a4bdc3cec7be7e101822666b895340a3d72e95546fb591b extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'stylesheets' => array($this, 'block_stylesheets'),
+            'navigation' => array($this, 'block_navigation'),
+            'body' => array($this, 'block_body'),
+            'sidebar' => array($this, 'block_sidebar'),
+            'javascripts' => array($this, 'block_javascripts'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "home/show.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "home/show.html.twig"));
+
+        // line 1
+        echo "
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv=\"Content-Type\" content=\"text/html\"; charset=utf-8\" />
+    <title>";
+        // line 8
+        $this->displayBlock('title', $context, $blocks);
+        echo " - Blog</title>
+    <!--[if lt IE 9]>
+    <script src=\"http://html5shim.googlecode.com/svn/trunk/html5.js\"></script>
+    <![endif]-->
+    ";
+        // line 12
+        $this->displayBlock('stylesheets', $context, $blocks);
+        // line 16
+        echo "    <link rel=\"shortcut icon\" href=\"";
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("favicon.ico"), "html", null, true);
+        echo "\" />
+</head>
+<body>
+
+<main id=\"wrapper\">
+
+
+    <aside id=\"left\">
+        <h1>VA Blog</h1>
+        <div class=\"top\">
+            ";
+        // line 26
+        $this->displayBlock('navigation', $context, $blocks);
+        // line 39
+        echo "        </div>
+
+
+
+
+
+    </aside>
+
+    <main>
+        ";
+        // line 48
+        $this->displayBlock('body', $context, $blocks);
+        // line 82
+        echo "    </main>
+
+    <aside id=\"right\">
+        ";
+        // line 85
+        $this->displayBlock('sidebar', $context, $blocks);
+        // line 99
+        echo "    </aside>
+
+
+</main>
+
+
+";
+        // line 105
+        $this->displayBlock('javascripts', $context, $blocks);
+        // line 106
+        echo "</body>
+</html>";
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 8
+    public function block_title($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "title"));
+
+        echo "VA Blog";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 12
+    public function block_stylesheets($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        // line 13
+        echo "        <link rel='stylesheet' href= \"";
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("css/basic-template.css"), "html", null, true);
+        echo "\">
+
+    ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 26
+    public function block_navigation($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "navigation"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "navigation"));
+
+        // line 27
+        echo "                <nav>
+                    <ul class=\"navigation\">
+                        <li><a href=\"/main\">Home</a></li>
+                        <li><a href=\"#\">About</a></li>
+                        <li> <a href = \"#\">Add a new</a></li>
+                        <li><a href=\"#\">Contact</a></li>
+                    </ul>
+                    Created by <a href=\"https://github.com/Leratony\">Leratony</a>
+
+
+                </nav>
+            ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 48
+    public function block_body($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+
+        // line 49
+        echo "
+        <div>
+            <div class=\"shopUnitName\">
+                <h2>Title of article</h2>
+            </div>
+            <div class = \"autor\">
+                <span>Author: John Doe</span>
+            </div>
+            <div class=\"shopUnitPrice\">
+                <span>Date: 01/01/2018</span>
+            </div>
+            <div class = \"time\">
+                <span>Time: 23:03:44 </span>
+            </div>
+            <div class=\"shopUnit\">
+                <img src=\"http://placehold.it/308x231\" />
+
+
+                <div class=\"shopUnitShortDesc\">
+
+                    Here will be a content of news <br>
+                    Here will be a content of news<br>
+                    Here will be a content of news<br>
+                    Here will be a content of news<br>
+
+                </div>
+                <a href=\"#\" class=\"shopUnitMore\">
+                    More...
+                </a>
+            </div>
+            <div class = \"comments_and_views\"> 5 comments 123 views </div>
+
+        ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 85
+    public function block_sidebar($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "sidebar"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "sidebar"));
+
+        // line 86
+        echo "            <form action=\"/main\" name = \"Enter\" method = \"post\">
+                <label class = \"Login\"> Log In:<br>
+                        <input type =\"text\" name = \"login\"/>
+                    </label><br>
+
+                <label class = \"Password1\"> Enter password:<br>
+                        <input type =\"password\" name = \"pswd1\"/>
+                    </label>
+
+
+                <input type = \"submit\" name = \"EnterSend\" value = \"Log In\">
+            </form>
+        ";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 105
+    public function block_javascripts($context, array $blocks = array())
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "javascripts"));
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "home/show.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  259 => 105,  237 => 86,  228 => 85,  186 => 49,  177 => 48,  156 => 27,  147 => 26,  133 => 13,  124 => 12,  106 => 8,  95 => 106,  93 => 105,  85 => 99,  83 => 85,  78 => 82,  76 => 48,  65 => 39,  63 => 26,  49 => 16,  47 => 12,  40 => 8,  31 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv=\"Content-Type\" content=\"text/html\"; charset=utf-8\" />
+    <title>{% block title %}VA Blog{% endblock %} - Blog</title>
+    <!--[if lt IE 9]>
+    <script src=\"http://html5shim.googlecode.com/svn/trunk/html5.js\"></script>
+    <![endif]-->
+    {% block stylesheets %}
+        <link rel='stylesheet' href= \"{{ asset('css/basic-template.css')}}\">
+
+    {% endblock %}
+    <link rel=\"shortcut icon\" href=\"{{ asset('favicon.ico') }}\" />
+</head>
+<body>
+
+<main id=\"wrapper\">
+
+
+    <aside id=\"left\">
+        <h1>VA Blog</h1>
+        <div class=\"top\">
+            {% block navigation %}
+                <nav>
+                    <ul class=\"navigation\">
+                        <li><a href=\"/main\">Home</a></li>
+                        <li><a href=\"#\">About</a></li>
+                        <li> <a href = \"#\">Add a new</a></li>
+                        <li><a href=\"#\">Contact</a></li>
+                    </ul>
+                    Created by <a href=\"https://github.com/Leratony\">Leratony</a>
+
+
+                </nav>
+            {% endblock %}
+        </div>
+
+
+
+
+
+    </aside>
+
+    <main>
+        {% block body %}
+
+        <div>
+            <div class=\"shopUnitName\">
+                <h2>Title of article</h2>
+            </div>
+            <div class = \"autor\">
+                <span>Author: John Doe</span>
+            </div>
+            <div class=\"shopUnitPrice\">
+                <span>Date: 01/01/2018</span>
+            </div>
+            <div class = \"time\">
+                <span>Time: 23:03:44 </span>
+            </div>
+            <div class=\"shopUnit\">
+                <img src=\"http://placehold.it/308x231\" />
+
+
+                <div class=\"shopUnitShortDesc\">
+
+                    Here will be a content of news <br>
+                    Here will be a content of news<br>
+                    Here will be a content of news<br>
+                    Here will be a content of news<br>
+
+                </div>
+                <a href=\"#\" class=\"shopUnitMore\">
+                    More...
+                </a>
+            </div>
+            <div class = \"comments_and_views\"> 5 comments 123 views </div>
+
+        {% endblock %}
+    </main>
+
+    <aside id=\"right\">
+        {% block sidebar %}
+            <form action=\"/main\" name = \"Enter\" method = \"post\">
+                <label class = \"Login\"> Log In:<br>
+                        <input type =\"text\" name = \"login\"/>
+                    </label><br>
+
+                <label class = \"Password1\"> Enter password:<br>
+                        <input type =\"password\" name = \"pswd1\"/>
+                    </label>
+
+
+                <input type = \"submit\" name = \"EnterSend\" value = \"Log In\">
+            </form>
+        {% endblock %}
+    </aside>
+
+
+</main>
+
+
+{% block javascripts %}{% endblock %}
+</body>
+</html>", "home/show.html.twig", "/home/valeria/projects/Blog/app/Resources/views/home/show.html.twig");
+    }
+}
