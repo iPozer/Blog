376f6637b202817fb3ee2a1121d4c986a2169934
Valeria Antonova <valeria.antonia001@gmail.com>
2018-04-19 19:40:23 +0600

    Создана сущность поста. Все настройки как у юзера. Сменен параметр у юзера

---
+++ src/VA/BlogBundle/Entity/Post.php
@@ -0,0 +1,241 @@
+<?php
+
+namespace VA\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="post")
+ */
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+
+    protected $id;
+
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $title;
+
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    protected $author;
+
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $post;
+
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $comments;
+
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    protected $image;
+
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $tags;
+
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $created_at;
+
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $updated_at;
+
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $slug;
+
+
+
+    
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param mixed $author
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPost()
+    {
+        return $this->post;
+    }
+
+    /**
+     * @param mixed $post
+     */
+    public function setPost($post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComments()
+    {
+        return $this->comments;
+    }
+
+    /**
+     * @param mixed $comments
+     */
+    public function setComments($comments)
+    {
+        $this->comments = $comments;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTags()
+    {
+        return $this->tags;
+    }
+
+    /**
+     * @param mixed $tags
+     */
+    public function setTags($tags)
+    {
+        $this->tags = $tags;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param mixed $created_at
+     */
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param mixed $updated_at
+     */
+    public function setUpdatedAt($updated_at)
+    {
+        $this->updated_at = $updated_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param mixed $slug
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    }
+
+
+
+}
--- src/VA/BlogBundle/Entity/User.php
+++ src/VA/BlogBundle/Entity/User.php
@@ -55,7 +55,7 @@ class User
 
 
     /**
-     * @ORM\Column(type="text")
+     * @ORM\Column(type="string")
      */
     protected $comments;
 
