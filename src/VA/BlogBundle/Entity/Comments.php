a5b1882fed56935c3d15364c2d5ddcbd33f16925
Valeria Antonova <valeria.antonia001@gmail.com>
2018-04-19 19:50:38 +0600

    К предыдущему

--- src/VA/BlogBundle/Entity/Comments.php
+++ src/VA/BlogBundle/Entity/Comments.php
@@ -1,7 +1,110 @@
 <?php
+
+namespace VA\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
 /**
- * Created by PhpStorm.
- * User: valeria
- * Date: 4/19/18
- * Time: 7:40 PM
+ * @ORM\Entity
+ * @ORM\Table(name="comments")
  */
+class Comments
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $author;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $post;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected $comment;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    protected $approved;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $created_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $updated_at;
+
+
+    
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPost()
+    {
+        return $this->post;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApproved()
+    {
+        return $this->approved;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+
+}
